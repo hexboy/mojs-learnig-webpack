65992c0be5dcf771aedbbd352afdb6b7fd099007
Hassan Yousefi <hexboy2011@gmail.com>
2017-10-06 16:08:36 +0330

    add mojs-player + simple rect animation

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,11 @@
 import mojs from 'mo-js';
+import MojsPlayer from 'mojs-player';
 
-var shape = new mojs.Shape({
+var rect1 = new mojs.Shape({
 	parent: '#mojs-container',
-	shape: 'circle',
-}).play();
+	shape: 'rect',
+	y: {[-300] : 0},
+	duration: 1000,
+});
+
+new MojsPlayer({add: rect1});
